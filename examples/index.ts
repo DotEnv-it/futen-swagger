2892f45d0be3d955252cee78561ea64aac3a4090
Brian Ferri <brian.ferri@dotenv.it>
2024-06-23 16:28:51 +0200

    feat: Remove unnecessary code and update SwaggerUI configuration

--- examples/index.ts
+++ examples/index.ts
@@ -1,16 +1,8 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import { Swagger, docs } from '../dist/index.mjs';
+import { Swagger } from '../dist/index.mjs';
 import Futen, { route } from 'futen';
 
 @route('/')
 class Home {
-    @docs({
-        responses: {
-            200: {
-                description: 'Hello, World!'
-            }
-        }
-    })
     public get(): Response {
         return new Response('Hello, World!');
     }
